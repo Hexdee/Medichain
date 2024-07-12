9c4a3ee46534629db94d9412b7b2600ecec03d9e
jibolagithub <141321183+jibolagithub@users.noreply.github.com>
2024-07-12 17:47:54 +0100

    Update footer.js

--- web-app/client/src/components/Homenewfiles/Home/footer.js
+++ web-app/client/src/components/Homenewfiles/Home/footer.js
@@ -15,8 +15,8 @@ class Footer extends Component {
                                     </a>
                                 </div>
                                 <p className="desc mb-30 text-white">
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                                    dinonumy eirmod tempor invidunt.
+                                    Mariam and Ajibola's project@Unilorin
+    
                                 </p>
                                 <ul className="socials">
                                     <li>
@@ -47,10 +47,10 @@ class Footer extends Component {
                             <div className="footer-widget">
                                 <h3>About Us</h3>
                                 <ul className="links">
-                                    <li><a href="">Home</a></li>
-                                    <li><a href="">Feature</a></li>
-                                    <li><a href="">About</a></li>
-                                    <li><a href="">Testimonials</a></li>
+                                    //<li><a href="">Home</a></li>
+                                    //<li><a href="">Feature</a></li>
+                                    //<li><a href="">About</a></li>
+                                    //<li><a href="">Testimonials</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -59,22 +59,22 @@ class Footer extends Component {
                             <div className="footer-widget">
                                 <h3>Features</h3>
                                 <ul className="links">
-                                    <li><a href="">How it works</a></li>
-                                    <li><a href="">Privacy policy</a></li>
-                                    <li><a href="">Terms of service</a></li>
-                                    <li><a href="">Refund policy</a></li>
+                                    //<li><a href="">How it works</a></li>
+                                    //<li><a href="">Privacy policy</a></li>
+                                    //<li><a href="">Terms of service</a></li>
+                                    //<li><a href="">Refund policy</a></li>
                                 </ul>
                             </div>
                         </div>
 
                         <div className="col-xl-3 col-lg-3 col-md-6">
                             <div className="footer-widget">
-                                <h3>Other Products</h3>
-                                <ul className="links">
-                                    <li><a href="">Accounting Software</a></li>
-                                    <li><a href="">Billing Software</a></li>
-                                    <li><a href="">Booking System</a></li>
-                                    <li><a href="">Tracking System</a></li>
+                                //<h3>Other Products</h3>
+                                //<ul className="links">
+                                    //<li><a href="">Accounting Software</a></li>
+                                    //<li><a href="">Billing Software</a></li>
+                                    //<li><a href="">Booking System</a></li>
+                                    //<li><a href="">Tracking System</a></li>
                                 </ul>
                             </div>
                         </div>
