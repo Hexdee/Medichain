835438b89a57940c714d97cba2bd5ea1ed0ddac2
jibolagithub <141321183+jibolagithub@users.noreply.github.com>
2024-07-12 20:16:34 +0100

    Update footer.js

--- web-app/client/src/components/Homenewfiles/Home/footer.js
+++ web-app/client/src/components/Homenewfiles/Home/footer.js
@@ -43,41 +43,9 @@ class Footer extends Component {
                             </div>
                         </div>
 
-                        <div className="col-xl-2 col-lg-2 col-md-6">
-                            <div className="footer-widget">
-                                <h3>About Us</h3>
-                                <ul className="links">
-                                    \\<li><a href="">Home</a></li>
-                                    \\<li><a href="">Feature</a></li>
-                                    \\<li><a href="">About</a></li>
-                                    \\<li><a href="">Testimonials</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="col-xl-3 col-lg-3 col-md-6">
-                            <div className="footer-widget">
-                                <h3>Features</h3>
-                                <ul className="links">
-                                    \\<li><a href="">How it works</a></li>
-                                    \\<li><a href="">Privacy policy</a></li>
-                                    \\<li><a href="">Terms of service</a></li>
-                                    \\<li><a href="">Refund policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="col-xl-3 col-lg-3 col-md-6">
-                            <div className="footer-widget">
-                                //<h3>Other Products</h3>
-                                //<ul className="links">
-                                    //<li><a href="">Accounting Software</a></li>
-                                    //<li><a href="">Billing Software</a></li>
-                                    //<li><a href="">Booking System</a></li>
-                                    //<li><a href="">Tracking System</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        
+           
+              </div>
                     </div>
                 </div>
             </div>
