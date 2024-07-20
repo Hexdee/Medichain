02ce4054c4aff08815c7675d0002d0a62cdc5a95
jibolagithub <141321183+jibolagithub@users.noreply.github.com>
2024-07-20 17:30:57 +0000

    Update Landingpage.js

--- web-app/client/src/components/Homenewfiles/Home/Landingpage.js
+++ web-app/client/src/components/Homenewfiles/Home/Landingpage.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import AppBar from './AppBar';
 import Sectionfirst from './section1';
 import Sectiontwo from './section2';
-// import Sectionthree from './section3';
-import Footer from './footer';
+import Sectionthree from './section3';
+//import Footer from './footer';
 
 
 class Landingpage extends Component {
