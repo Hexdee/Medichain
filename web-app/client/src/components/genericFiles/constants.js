b302e4e8a4387b47905b0146f51ad1751ef982b7
Lukman Saheed <lukmansaheed79@gmail.com>
2024-07-28 06:47:41 -0400

    Update constants.js

--- web-app/client/src/components/genericFiles/constants.js
+++ web-app/client/src/components/genericFiles/constants.js
@@ -1,4 +1,4 @@
-const ADDRESS = `https://4000-hexdee-medichain-5qml707vhu3.ws-eu115.gitpod.io`;
+const ADDRESS = `https://4000-hexdee-medichain-5qml707vhu3.ws-eu115.gitpod.io/`;
 const drawerWidth = 240;
 const NAV_WIDTH = 280;
 const HEADER_MOBILE = 64;
